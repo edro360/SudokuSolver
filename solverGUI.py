c4e67971ae394409ae337529d846e1ada9101c88
edro360 <redwin925a@gmail.com>
2022-12-31 00:23:31 -0800

    Added QoL improvements to the GUI.

--- solverGUI.py
+++ solverGUI.py
@@ -7,8 +7,9 @@ pygame.init()
 
 # ----- Pygame Window -----
 WINDOW_WIDTH = 540
-WINDOW_HEIGHT = 590
+WINDOW_HEIGHT = 600
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Sudoku Solver")
 
 # ----- Pygame Solve Button -----
 SOLVE_WIDTH = 100
@@ -30,7 +31,7 @@ font = pygame.font.Font(None, 36)
 
 def drawGrid():
     # Draw the horizontal lines
-    for i in range(1, 9):
+    for i in range(1, 10):
         pygame.draw.line(window, GRAY, (0, i * 60), (540, i * 60), 2)
     # Draw the vertical lines
     for i in range(1, 9):
@@ -42,17 +43,17 @@ def drawNums(board):
             # If the cell is not empty, draw the number
             if board[i][j] != 0:
                 text = font.render(str(board[i][j]), True, BLACK)
-                window.blit(text, (j * 60 + 20, i * 60 + 20))
+                window.blit(text, (j * 60 + 25, i * 60 + 20))
 
-def drawSolve():
-    pygame.draw.rect(window, BLACK, (SOLVE_X, SOLVE_Y, SOLVE_WIDTH, SOLVE_HEIGHT))
-    text = font.render("Solve", True, WHITE)
-    window.blit(text, (SOLVE_X + 20, SOLVE_Y + 10))
+def drawBigButton(buttonX, buttonY, buttonWidth, buttonHeight, msg):
+    pygame.draw.rect(window, BLACK, (buttonX, buttonY, buttonWidth, buttonHeight))
+    text = font.render(msg, True, WHITE)
+    window.blit(text, (buttonX + 20, buttonY + 10))
 
-def drawReset():
-    pygame.draw.rect(window, BLACK, (RESET_X, RESET_Y, RESET_WIDTH, RESET_HEIGHT))
-    text = font.render("Reset", True, WHITE)
-    window.blit(text, (RESET_X + 20, RESET_Y + 10))
+def isWithin(mouseX, mouseY, buttonX, buttonY, buttonHeight, buttonWidth):
+    if (buttonX <= mouseX <= buttonX+buttonWidth and buttonY <= mouseY <= buttonY+buttonHeight):
+        return True
+    return False
 
 def main():
     # Initialize the board
@@ -74,40 +75,42 @@ def main():
         window.fill(WHITE)
         # Handle events
         for event in pygame.event.get():
-            # If the user closes the window, set the running flag to False
-            if event.type == pygame.QUIT:
-                running = False
-            # If the user clicks the mouse, get the cell coordinates and update the board
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x, mouse_y = event.pos
-                row = mouse_y // 60
-                col = mouse_x // 60
-                if row < 9 and col < 9:
-                    board[row][col] = (board[row][col] + 1) % 10
-                elif SOLVE_X <= mouse_x <= SOLVE_X + SOLVE_WIDTH and SOLVE_Y <= mouse_y <= SOLVE_Y + SOLVE_HEIGHT:
-                    solveDFS(board)
-                elif RESET_X <= mouse_x <= RESET_X + RESET_WIDTH and RESET_Y <= mouse_y <= RESET_Y + RESET_HEIGHT:
-                    board = [
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            ]
+            match event.type:
+                # If the user closes the window, set the running flag to False
+                case pygame.QUIT:
+                    running = False
+                # If the user clicks the mouse, get the cell coordinates and update the board
+                case pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = event.pos
+                    row = mouse_y // 60
+                    col = mouse_x // 60
+                    if row < 9 and col < 9:
+                        board[row][col] = (board[row][col] + 1) % 10
+                    elif isWithin(mouse_x, mouse_y, SOLVE_X, SOLVE_Y, SOLVE_HEIGHT, SOLVE_WIDTH):
+                        pygame.display.set_caption("Sudoku Solver using DFS")
+                        solveDFS(board)
+                    elif isWithin(mouse_x, mouse_y, RESET_X, RESET_Y, RESET_HEIGHT, RESET_WIDTH):
+                        pygame.display.set_caption("Sudoku Solver")
+                        board = [
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                ]
         # Draw the grid and the numbers
         drawGrid()
         drawNums(board)
-        drawSolve()
-        drawReset()
+        drawBigButton(SOLVE_X, SOLVE_Y, SOLVE_WIDTH, SOLVE_HEIGHT, "Solve")
+        drawBigButton(RESET_X, RESET_Y, RESET_WIDTH, RESET_HEIGHT, "Reset")
         # Update the display
         pygame.display.update()
     # Quit pygame
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
